22be351098f56244a47371f8ab3e865c2ef701e6
dz <dddzzz999@yahoo.com>
2018-05-20 11:58:36 -0400

    init

---
+++ src/main/java/com/dz/cfgserverclient/Springconfigserver2ClientApplication.java
@@ -0,0 +1,12 @@
+package com.dz.cfgserverclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springconfigserver2ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springconfigserver2ClientApplication.class, args);
+	}
+}
